775aec4c91834000159d1034add621c8052975c9
Nikita Masych <nikitamasich152@gmail.com>
2023-11-29 23:51:13 +0200

    improved template

--- mongo-schema/schema.js
+++ mongo-schema/schema.js
@@ -1,11 +1,11 @@
-// Create a sample collection
+// Switch to our DB inside mongo cluster: 
 const initialDB = db.getSiblingDB('lab2');
 
+// Create a sample collection
 initialDB.createCollection("users");
 
 // Insert some sample data into the collection
 initialDB.users.insertMany([
   { username: "john_doe", email: "john.doe@example.com", age: 25 },
   { username: "jane_smith", email: "jane.smith@example.com", age: 30 },
-  // Add more documents as needed
 ]);
